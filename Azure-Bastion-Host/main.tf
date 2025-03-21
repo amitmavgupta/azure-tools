1dd4e0abec16755022a8f43c9823120b61161985
amitmavgupta <115551423+amitmavgupta@users.noreply.github.com>
2025-03-21 18:05:19 +0530

    Updated with Fixes

--- Azure-Bastion-Host/main.tf
+++ Azure-Bastion-Host/main.tf
@@ -1,31 +1,34 @@
-
 # Create Resource Group
-resource "azurerm_resource_group" "azurebastion" {
-  name     = "azurevm"
-  location = "canadacentral"
+resource "random_pet" "rg_name" {
+  prefix = var.resource_group_name_prefix
+}
+
+resource "azurerm_resource_group" "rg" {
+  location = var.resource_group_location
+  name     = random_pet.rg_name.id
 }
 
 # Create Virtual Network
 resource "azurerm_virtual_network" "vnet" {
-  name                = "azure-vnet"
+  name                = "azure-network"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.azurebastion.location
-  resource_group_name = azurerm_resource_group.azurebastion.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 }
 
 # Create Subnet for Azure Bastion
 resource "azurerm_subnet" "bastion_subnet" {
   name                 = "AzureBastionSubnet"
-  resource_group_name  = azurerm_resource_group.azurebastion.name
+  resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
 # Create Public IP for Azure Bastion
 resource "azurerm_public_ip" "bastion_pip" {
-  name                = "azure-bastion-pip"
-  location            = azurerm_resource_group.azurebastion.location
-  resource_group_name = azurerm_resource_group.azurebastion.name
+  name                = "azure-pip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Static"
   sku                 = "Standard"
 }
@@ -33,8 +36,8 @@ resource "azurerm_public_ip" "bastion_pip" {
 # Create Azure Bastion Host
 resource "azurerm_bastion_host" "bastion" {
   name                = "azure-bastion"
-  location            = azurerm_resource_group.azurebastion.location
-  resource_group_name = azurerm_resource_group.azurebastion.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
     name                 = "configuration"
